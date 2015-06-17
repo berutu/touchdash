28b8b49bdc95277de774fbf348124cc53925b84a
Katsuhiro Suzuki <berutu0106@gmail.com>
2015-06-17 22:29:27 +0900

    スタミナの追加 定数の整理

--- Assets/Scripts/PlayerParty.cs
+++ Assets/Scripts/PlayerParty.cs
@@ -3,26 +3,30 @@ using System.Collections;
 
 public class PlayerParty : MonoBehaviour {
 
+	public const int MAXSTAMINA = 1000;
+	public const float FORWARDSPEED = 0.02f;
+	public const float BACKSPEED = 0.01f;
+	public const float CHANGETIME = 0.01f;
+	public const int RECEDESTAMINA = 3;
+
 	public int membership;
 	public Vector2 offset;
-	public float moveSpeed;
-	public float recedeSpeed;
+	public int stamina;
 
 	// Use this for initialization
 	void Start () {
 		membership = transform.childCount;
 		FormationChange();
 		offset = new Vector2 (0f, 0f);
-		moveSpeed = 0.02f;
-		recedeSpeed = 0.01f;
+		stamina = MAXSTAMINA;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.touchCount > 0 || Input.GetMouseButton (0)) {
+		if ((Input.touchCount > 0 || Input.GetMouseButton (0)) && stamina >= 0) {
 			Forward ();
 		} else if (offset.y >= 0f) {
-			offset.y -= recedeSpeed;
+			Back ();
 		}
 
 		if (membership != transform.childCount) {
@@ -30,40 +34,46 @@ public class PlayerParty : MonoBehaviour {
 			FormationChange();		
 		}
 		transform.position = new Vector3 (offset.x, offset.y, 0f);
-		//transform.localPosition.Set(offset.x, offset.y, offset.z);
+		Debug.Log (stamina);
 	}
 
 	public void Forward(){
-		offset.y += moveSpeed;
+		offset.y += FORWARDSPEED;
+		stamina -= RECEDESTAMINA;
+	}
+
+	public void Back(){
+		offset.y -= BACKSPEED;
+		if(stamina <= MAXSTAMINA)
+			stamina++;
 	}
 
 	public void FormationChange(){
-		float changeTime = 1.0f;
 		switch (membership) {
 		case 1:
-			iTween.MoveTo(transform.GetChild(0).gameObject, new Vector3(-0.3f, -3f, 0f), changeTime);
+			iTween.MoveTo(transform.GetChild(0).gameObject, new Vector3(-0.3f, -3f, 0f), CHANGETIME);
 			break;
 		case 2:
-			iTween.MoveTo(transform.GetChild(0).gameObject, new Vector3(0.1f, -3f, 0f), changeTime);
-			iTween.MoveTo(transform.GetChild(1).gameObject, new Vector3(-0.7f, -3f, 0f), changeTime);
+			iTween.MoveTo(transform.GetChild(0).gameObject, new Vector3(0.1f, -3f, 0f), CHANGETIME);
+			iTween.MoveTo(transform.GetChild(1).gameObject, new Vector3(-0.7f, -3f, 0f), CHANGETIME);
 			break;
 		case 3:
-			iTween.MoveTo(transform.GetChild(0).gameObject, new Vector3(-0.3f, -3f, 0f), changeTime);
-			iTween.MoveTo(transform.GetChild(1).gameObject, new Vector3(0.5f, -3f, 0f), changeTime);
-			iTween.MoveTo(transform.GetChild(2).gameObject, new Vector3(-1.1f, -3f, 0f), changeTime);
+			iTween.MoveTo(transform.GetChild(0).gameObject, new Vector3(-0.3f, -3f, 0f), CHANGETIME);
+			iTween.MoveTo(transform.GetChild(1).gameObject, new Vector3(0.5f, -3f, 0f), CHANGETIME);
+			iTween.MoveTo(transform.GetChild(2).gameObject, new Vector3(-1.1f, -3f, 0f), CHANGETIME);
 			break;
 		case 4:
-			iTween.MoveTo(transform.GetChild(0).gameObject, new Vector3(-0.3f, -3f, 0f), changeTime);
-			iTween.MoveTo(transform.GetChild(1).gameObject, new Vector3(0.5f, -3f, 0f), changeTime);
-			iTween.MoveTo(transform.GetChild(2).gameObject, new Vector3(-1.1f, -3f, 0f), changeTime);
-			iTween.MoveTo(transform.GetChild(3).gameObject, new Vector3(-0.3f, -2.1f, 0f), changeTime);
+			iTween.MoveTo(transform.GetChild(0).gameObject, new Vector3(-0.3f, -3f, 0f), CHANGETIME);
+			iTween.MoveTo(transform.GetChild(1).gameObject, new Vector3(0.5f, -3f, 0f), CHANGETIME);
+			iTween.MoveTo(transform.GetChild(2).gameObject, new Vector3(-1.1f, -3f, 0f), CHANGETIME);
+			iTween.MoveTo(transform.GetChild(3).gameObject, new Vector3(-0.3f, -2.1f, 0f), CHANGETIME);
 			break;
 		case 5:
-			iTween.MoveTo(transform.GetChild(0).gameObject, new Vector3(-0.3f, -3f, 0f), changeTime);
-			iTween.MoveTo(transform.GetChild(1).gameObject, new Vector3(0.5f, -3f, 0f), changeTime);
-			iTween.MoveTo(transform.GetChild(2).gameObject, new Vector3(-1.1f, -3f, 0f), changeTime);
-			iTween.MoveTo(transform.GetChild(3).gameObject, new Vector3(0.1f, -2.1f, 0f), changeTime);
-			iTween.MoveTo(transform.GetChild(4).gameObject, new Vector3(-0.7f, -2.1f, 0f), changeTime);
+			iTween.MoveTo(transform.GetChild(0).gameObject, new Vector3(-0.3f, -3f, 0f), CHANGETIME);
+			iTween.MoveTo(transform.GetChild(1).gameObject, new Vector3(0.5f, -3f, 0f), CHANGETIME);
+			iTween.MoveTo(transform.GetChild(2).gameObject, new Vector3(-1.1f, -3f, 0f), CHANGETIME);
+			iTween.MoveTo(transform.GetChild(3).gameObject, new Vector3(0.1f, -2.1f, 0f), CHANGETIME);
+			iTween.MoveTo(transform.GetChild(4).gameObject, new Vector3(-0.7f, -2.1f, 0f), CHANGETIME);
 			break;
 		}
 	}
