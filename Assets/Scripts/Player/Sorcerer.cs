e42973b52f76349d7ca351bd5af5a456a635e2c6
Katsuhiro Suzuki <berutu0106@gmail.com>
2015-07-05 19:17:31 +0900

    SorcererとThunderを追加しました

--- Assets/Scripts/Player/Sorcerer.cs
+++ Assets/Scripts/Player/Sorcerer.cs
@@ -5,7 +5,9 @@ public class Sorcerer : PlayerBase {
 
 	// Use this for initialization
 	void Start () {
-	
+		attackingSpeed = 0.35f;
+		intervalTime = 3.0f;
+		StartCoroutine ("Attack");
 	}
 	
 	// Update is called once per frame
@@ -16,12 +18,18 @@ public class Sorcerer : PlayerBase {
 	#region implemented abstract members of PlayerBase
 	public override IEnumerator Attack ()
 	{
+		yield return new WaitForSeconds (0.1f);
 		while (true) {
-			attackEffect.SetActive (true);
+			attackEffect.GetComponent<BoxCollider2D>().enabled = true;
+			attackEffect.GetComponent<SpriteRenderer>().enabled = true;
+			attackEffect.GetComponent<Animator>().SetBool("Start", true);
 			yield return new WaitForSeconds (attackingSpeed);
-			attackEffect.SetActive (false);
+			attackEffect.GetComponent<BoxCollider2D>().enabled = false;
+			attackEffect.GetComponent<SpriteRenderer>().enabled = false;
+			attackEffect.GetComponent<Animator>().SetBool("Start", false);
 			yield return new WaitForSeconds (intervalTime);
 		}
+		yield return 0;
 	}
 
 	public override void Damage ()
